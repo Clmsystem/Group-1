f5c3c259cc393d7f3a3b99eea0bfd1777f649e5f
newzyz <nooknet102@gmail.com>
2021-03-09 12:45:23 +0700

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# Product
+
+test notify
---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('login');
+});
+
+// Route::post('/login', [LoginController::class, 'index'])->name('login');
+
+
+// Route::post('/Valid', [LoginController::class,'index'],function ($argv){
+
+// } )->name('valid');
+
+
+Route::post('/index', function () {
+    return view('index');
+})->name('/');
+
+Route::get('/section_one', [ObjectGroup1::class,'index']);
+Route::get('/section_one/{id}', [Kr::class,'index']);
