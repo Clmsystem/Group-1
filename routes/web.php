a4904f2a3508c384f7ba3ea737f8e03b444da286
newzyz <nooknet102@gmail.com>
2021-03-16 14:57:25 +0700

    hard reset

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ObjectGroup1;
+use App\Http\Controllers\Kr;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,41 +16,21 @@ use App\Http\Controllers\LoginController;
 */
 
 Route::get('/', function () {
-    return view('index');
+    return view('login');
 });
 
 // Route::post('/login', [LoginController::class, 'index'])->name('login');
 
 
-Route::post('/show', [LoginController::class,'index'],function ($log){
+// Route::post('/Valid', [LoginController::class,'index'],function ($argv){
 
-} )->name('show');
+// } )->name('valid');
 
 
+Route::post('/index', function () {
+    return view('index');
+})->name('/');
 
-<<<<<<< HEAD
-Route::get('/login1', function () {
-    return view('manage.login');
-});
-
-Route::get('/login2', function () {
-    return view('promote.login');
-});
-
-Route::get('/section_one', function () {
-    return view('section_one.content');
-});
-Route::get('/section_one/{objective_name}', function ($objective_name) {
-    return view('section_one.objective',['objective_name'=>$objective_name]);
-});
-=======
 Route::get('/section_one', [ObjectGroup1::class,'index']);
 Route::get('/section_one/{id}', [Kr::class,'index']);
-<<<<<<< HEAD
-<<<<<<< HEAD
 Route::post('/section_one/add', [ObjectGroup1::class,'addObject'])->name('addobject');
->>>>>>> d523ecd5cdd92796bc81510895b4072513eafb40
-=======
->>>>>>> parent of d523ecd... add
-=======
->>>>>>> parent of d523ecd... add
